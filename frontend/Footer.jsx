1d2c165d53f340caadaf3f76e67a06865ceef0ef
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2026-07-20 17:24:40 +0530

    redirect add

--- frontend/Footer.jsx
+++ frontend/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
 
-    const owner = import.meta.env.VITE_FOOTER_NAME || "Trisha & Vishesh"
+    const owner = import.meta.env.VITE_FOOTER_NAME;
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-[#0F172A] text-white text-xs sm:text-sm font-semibold z-50 border-t border-slate-800/50 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-center sm:justify-end">
